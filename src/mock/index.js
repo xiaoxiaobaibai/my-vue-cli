ecea1df08dbe61a6a0acebedd5c34bbda6d6e78c
xiaoxiaobaibai <hufan0929@126.com>
2022-06-26 21:45:04 +0800

    update: 将onBeforeSetupMiddleware修改为setupMiddlewares

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,3 +1,4 @@
-module.exports = {
+const obj = {
   '/service/a': './api/service.json'
 }
+export default obj
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 // console.log('(process.env', process.env)
-// const mockProxy = require('@/mock')
+const mockProxy = import('@/mock/index.js')
+console.log('mockProxy', mockProxy)
 module.exports = defineConfig({
   lintOnSave:false,
   transpileDependencies: true,
   devServer: {
-    onBeforeSetupMiddleware: function(devServer) {
+    setupMiddlewares: (middlewares, devServer) => {
       if (!devServer) {
         throw new Error('webpack-dev-server is not defined');
       }
 
-      devServer.app.post('/service/a', function (req, res) {
-        res.json({ custom: 'response' });
+      devServer.app.post('/service/a', (_, response) => {
+        response.send({ custom: 'response1' });
       });
+
+      return middlewares
     }
   }
 })
